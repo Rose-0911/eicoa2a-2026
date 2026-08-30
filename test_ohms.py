b322fa8147bc5237e9770ab9499dfde32b09c50d
Rose-0911 <226022809@edu.vut.ac.za>
2026-08-30 18:05:20 +0200

    Exercise 1

--- test_ohms.py
+++ test_ohms.py
@@ -9,3 +9,13 @@ assert calc_resistance(9, 0.03) == 300
 
 assert calc_resistance(24, 2) == 12
 print(calc_resistance.__doc__)
+
+from ohms_law import calc_current, calc_power
+
+power1 = calc_power(12, 6)
+print("power 1 =", power1, "W")
+
+power2 = calc_power(24, 12)
+print("power 2 =", power2, "W")
+
+print(calc_power.__doc__)
